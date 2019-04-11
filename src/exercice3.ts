fd9a7abcf364e6828862c9b1dbfdfb2225d29fbc
ahmed.regaieg <ahmed.rgaieg@gmail.com>
2019-04-11 12:58:49 +0200

    feat invert exercice2 and exercice3

--- src/exercice3.ts
+++ src/exercice3.ts
@@ -1,176 +1,163 @@
 /**
- * On considère les types suivants
+ * On considère l'objet de référence suivant
  */
-type Carnivore = 'CARNIVORE';
-type Herbivore = 'HERBIVORE';
-type Omnivore = 'OMNIVORE';
-type RegimeAlimentaire = Carnivore | Herbivore | Omnivore;
 
-interface Chat {
-  age: number;
-  longivite: number;
-  couleurYeux: string;
-  couleurPoiles: string;
-  nomProprietaire: string;
-  regimeAlimentaire: Carnivore;
-  chasse(): void;
-  miole(): void;
-}
-
-interface Chien {
-  age: number;
-  longivite: number;
-  couleurYeux: string;
-  couleurPoiles: string;
-  nomProprietaire: string;
-  regimeAlimentaire: Carnivore;
-  chasse(): void;
-  aboie(): void;
-}
-
-interface Lion {
-  age: number;
-  longivite: number;
-  couleurYeux: string;
-  couleurPoiles: string;
-  regimeAlimentaire: Carnivore;
-  chasse(): void;
-  rugit(): void;
-}
-
-interface Loup {
-  age: number;
-  longivite: number;
-  couleurYeux: string;
-  couleurPoiles: string;
-  regimeAlimentaire: Carnivore;
-  chasse(): void;
-  hurle(): void;
-}
-
-interface Coq {
-  age: number;
-  longivite: number;
-  couleurYeux: string;
-  couleurPlumes: string;
-  regimeAlimentaire: Omnivore;
-  nomProprietaire: string;
-  chante(): void;
-}
-
-interface Aigle {
-  age: number;
-  longivite: number;
-  couleurYeux: string;
-  couleurPlumes: string;
-  regimeAlimentaire: Carnivore;
-  chasse(): void;
-  glatit(): void;
-}
-
-interface Mouton {
-  age: number;
-  longivite: number;
-  couleurYeux: string;
-  couleurLaine: string;
-  regimeAlimentaire: Herbivore;
-  nomProprietaire: string;
-  bele(): void;
-}
+const referenceObject = {
+  numberValue: 5,
+  stringValue: 'string',
+  booleanValue: true,
+};
 
 /**
  * Question 1:
- * Créer les types
- * - Felin, Canide, Oiseau, Caprine
- * - Chasseur, Chasse
- * - Animal
- * en utilisant les types ci-dessus
+ * Remplacer le type any de sorte que la fonction takeSameTypeAsReferenceObject
+ * n'accepte des paramètres que de même type que referenceObject
  */
+function takeSameTypeAsReferenceObject(parameter: any): void {
+  console.log(`${parameter} has the same type as referenceObject`);
+}
 
-type Felin = any;
-type Canide = any;
-type Oiseau = any;
-type Caprine = any;
+// OK till question 4 is done
+takeSameTypeAsReferenceObject({
+  numberValue: 7,
+  stringValue: 'ok',
+  booleanValue: false,
+});
 
-type Chasseur = any;
-type Chasse = any;
+// ERROR
+takeSameTypeAsReferenceObject({ numberValue: 2, stringValue: 'ko' });
 
-type Animal = any;
+// ERROR
+takeSameTypeAsReferenceObject({
+  numberValue: 9,
+  stringValue: 'ko',
+  booleanValue: false,
+  unknownField: 'cause of the error',
+});
 
 /**
  * Question 2:
- * Renomer le champ age en ageEstime pour tous les animaux
- * Indication: Penser à l'héritage avec extend
+ * Remplacer le type any de sorte que la fonction takeKeyOfReferenceObject
+ * n'accepte que des paramètres correspondant aux champs de referenceObject
  */
+function takeKeyOfReferenceObject(parameter: any): void {
+  console.log(`${parameter} is one of referenceObject keys`);
+}
+
+// OK
+takeKeyOfReferenceObject('numberValue');
+
+// ERROR
+takeKeyOfReferenceObject('unknownField');
 
 /**
  * Question 3:
- * Caster l'objet chat tout en gardant son type Animal de sorte que
- * - chat.miole() compile
- * - chat.glatit() ne compile pas
+ * Remplacer le type any de sorte que la fonction takeSameTypeAsReferenceObjectWithoutBoolean
+ * n'accepte des paramètres que de même type que referenceObject sans le champs booleanValue
  */
-
-function knownType() {
-  const chat: Animal = {
-    age: 2,
-    longivite: 16,
-    couleurPoiles: 'gris',
-    couleurYeux: 'bleu',
-    nomProprietaire: 'Ahmed',
-    regimeAlimentaire: 'CARNIVORE',
-    chasse: () => {
-      console.log('chasse souris, reptiles et oiseaux');
-    },
-    miole: () => {
-      console.log('souvent');
-    },
-  };
-
-  // ok
-  chat.miole();
-
-  // error
-  chat.glatit();
+function takeSameTypeAsReferenceObjectWithoutBoolean(parameter: any): void {
+  console.log(
+    `${parameter} has the same type as referenceObject without booleanValue`,
+  );
 }
+
+// OK till question 4 is done
+takeSameTypeAsReferenceObjectWithoutBoolean({
+  numberValue: 4,
+  stringValue: 'ok',
+});
+
+// ERROR
+takeSameTypeAsReferenceObjectWithoutBoolean({
+  numberValue: 4,
+  stringValue: 'ko',
+  boolValue: true,
+});
 
 /**
  * Question 4:
- * Tout en gardant Animal comme type d'animal
- * Faire en sorte que animal.chasse() compile
+ * Remplacer le type any de sorte que la fonction takeKeyOfReferenceObjectExceptBoolean
+ * n'accepte que des paramètres correspondant aux champs de referenceObject sauf booleanValue
  */
-function callChasse(animal: Animal): void {
-  if (true /** replace true to check type */) {
-    animal.chasse();
-
-    // Type '"HERBIVORE"' is not assignable to type '"CARNIVORE"'
-    animal.regimeAlimentaire = 'HERBIVORE';
-  }
+function takeKeyOfReferenceObjectExceptBoolean(parameter: any): void {
+  console.log(
+    `${parameter} is one of referenceObject keys except booleanValue`,
+  );
 }
+
+// OK
+takeKeyOfReferenceObjectExceptBoolean('numberValue');
+
+// ERROR
+takeKeyOfReferenceObjectExceptBoolean('booleanValue');
+
+/**
+ * Question 4:
+ * Ajouter à l'objet referenceObject le champ numberValue2 avec la valeur 100
+ * Faire en sorte que les appels aux fonctions
+ * takeKeyOfReferenceObject, takeSameTypeAsReferenceObjectWithoutBoolean
+ * takeKeyOfReferenceObject et takeKeyOfReferenceObjectExceptBoolean
+ * suivants continuent à compiler
+ */
+
+takeSameTypeAsReferenceObject({
+  numberValue: 7,
+  stringValue: 'ok',
+  booleanValue: false,
+  numberValue2: 0x45,
+});
+takeSameTypeAsReferenceObjectWithoutBoolean({
+  numberValue: 7,
+  stringValue: 'ok',
+  numberValue2: 0x45,
+});
+
+takeKeyOfReferenceObject('numberValue2');
+takeKeyOfReferenceObjectExceptBoolean('numberValue2');
 
 /**
  * Question 5:
- * Ajouter le champs sousFamille à tous les types d'animaux
- * qui a pour valeurs possibles:
- * FELIN, CANIDE, OISEAU, CAPRINE
- * en fonction de l'animal
+ * Modifier le typage des fonctions
+ * takeKeyOfReferenceObject, takeSameTypeAsReferenceObjectWithoutBoolean
+ * takeKeyOfReferenceObject et takeKeyOfReferenceObjectExceptBoolean
+ * de sorte qu'elles continuent à respecter leurs spec quel que soit le champ
+ * ajouté à referenceObject
+ * Tester après modification en ajoutant un champ stringValue2 à referenceObject
+ * Les appels suivants doivent compiler sans aucune autre modification
  */
+
+takeSameTypeAsReferenceObject({
+  numberValue: 7,
+  stringValue: 'ok',
+  booleanValue: false,
+  numberValue2: 0x45,
+  stringValue2: 'ok ok',
+});
+takeSameTypeAsReferenceObjectWithoutBoolean({
+  numberValue: 7,
+  stringValue: 'ok',
+  numberValue2: 0x45,
+  stringValue2: 'ok ok',
+});
+
+takeKeyOfReferenceObject('stringValue2');
+takeKeyOfReferenceObjectExceptBoolean('stringValue2');
 
 /**
  * Question 6:
- * Modifier la fonction suivante pour bien afficher la couleur de la robe de l'animal
- * Indication: Penser à définir isTypeFunction
+ * Renommer le champ booleanValue en boolValue
+ * Les appels suivants NE doivent PAS compiler
+ */
+takeKeyOfReferenceObjectExceptBoolean('boolValue');
+takeKeyOfReferenceObjectExceptBoolean('booleanValue');
+
+/**
+ * Question 7:
+ * Modifier le type de paramètre de takeKeyOfReferenceObjectExceptBoolean
+ * de sorte que le renommage de booleanValue lève une erreur dans la définition du type
  */
 
-function afficherLaCouleurDeLaRobe(animal: Animal) {
-  if (true) {
-    console.log(animal.couleurPoiles);
-  }
-
-  if (true) {
-    console.log(animal.couleurPlumes);
-  }
-
-  if (true) {
-    console.log(animal.couleurLaine);
-  }
-}
+/**
+ * Question 8:
+ * Implémenter un type générique Omit<T, U> qui sera égal à T sans les champs U
+ */
